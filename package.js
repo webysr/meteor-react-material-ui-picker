272eb2650ea4d2fdd03002893ea0e830cd1b741d
Jürgen Schlieber <juergen.schlieber@gmail.com>
2016-03-09 23:05:37 +0100

    Add summary and version constraints to package.js

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'webysr:react-material-ui-picker',
   version: '1.0.0',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'A simple file picker for Google Drive made with React and Material UI',
+  git: 'https://github.com/webysr/meteor-react-material-ui-picker.git',
   documentation: 'README.md'
 });
 
@@ -15,12 +11,10 @@ Package.onUse(function(api) {
   api.use('ecmascript@0.4.0-beta.11');
   api.use('mongo@1.1.4-beta.11');
   api.use('react-meteor-data@0.2.5');
-  api.use('webysr:googleapis-pub');
+  api.use('webysr:googleapis-pub@1.0.0');
 
-  //api.addFiles(['lib/FilePicker.jsx']);
   api.addFiles(['server/driveFilesPub.js'], 'server');
   api.mainModule('client/FilePicker.jsx', 'client');
-  //api.export('FilePicker');
 });
 
 Package.onTest(function(api) {
